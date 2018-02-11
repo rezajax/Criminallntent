4a40d727bec476c9d9e304c6d40187de085b0bef
Reza <mr1374h@gmail.com>
2018-02-12 02:36:02 +0330

    Edit CrimeLab

--- app/src/main/java/com/rezajax/criminalintent/CrimeLab.java
+++ app/src/main/java/com/rezajax/criminalintent/CrimeLab.java
@@ -1,8 +1,40 @@
 package com.rezajax.criminalintent;
 
-/**
- * Created by JAX on 2/12/2018.
- */
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+    private List<Crime> mCrimes;
+
+    public static CrimeLab get(Context context){
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+
+    private CrimeLab(Context context) {
+        mCrimes = new ArrayList<>();
+        for (int i = 0 ;i < 100 ; i++) {
+            Crime crime = new Crime();
+            crime.setmTitle("Crime #: + i");
+            crime.setmSolved(i % 2 == 0);
+            mCrimes.add(crime);
+        }
+    }
+
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime crime : mCrimes) {
+            return crime;
+        }
+        return null;
+    }
 }
