fcb49385dac92cbccf87bebd104ecaa37cb7dca8
Reza <mr1374h@gmail.com>
2018-02-12 03:08:05 +0330

    SingleFragmentActivity

--- app/src/main/java/com/rezajax/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/rezajax/criminalintent/CrimeActivity.java
@@ -1,26 +1,14 @@
 package com.rezajax.criminalintent;
 
-import com.rezajax.criminalintent.CrimeFragment;
-
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CrimeActivity extends AppCompatActivity {
+public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
-
-        FragmentManager fm = getFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container); //only for check
-
-        if (fragment == null) {
-            fragment = new CrimeFragment();
-            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
-
-        }
+    protected Fragment creatFragment() {
+        return new CrimeFragment();
     }
 }
---
+++ app/src/main/java/com/rezajax/criminalintent/SingleFragmentActivity.java
@@ -0,0 +1,26 @@
+package com.rezajax.criminalintent;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container); //only for check
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+
+        }
+    }
+}
+}
